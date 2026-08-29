86c7d1814b390c29b0a00a3994b2ec26632ac210
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2026-08-29 15:43:57 -0300

    juanma

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GrowInversiones/', // ⬅️ ¡REEMPLAZÁ ACÁ con el nombre exacto de tu repositorio en GitHub!
+  base: '/', // ⬅️ ¡REEMPLAZÁ ACÁ con el nombre exacto de tu repositorio en GitHub!
 })
